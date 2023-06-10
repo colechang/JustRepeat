08cc95253bb12c606386da64bafadaf8115ffa57
ColeChang <colechang10@gmail.com>
2023-06-10 00:26:27 -0400

    fixed page refresh bug

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId,tab)=>{
-    if(changeInfo.status === 'complete' && tab.url && tab.url.includes("youtube.com/watch")){
+    if(tab.url && tab.url.includes("youtube.com/watch")){
         const queryParameters = tab.url.split("?")[1];
         const urlParameters= new URLSearchParams(queryParameters);
 
